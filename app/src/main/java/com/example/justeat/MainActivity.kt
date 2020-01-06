72c095e197bee54deb5d1b1114fba2399000e5e7
ooizhengyi <jerryozy99@gmail.com>
2020-01-07 02:59:51 +0800

    mobileASSM

--- app/src/main/java/com/example/justeat/MainActivity.kt
+++ app/src/main/java/com/example/justeat/MainActivity.kt
@@ -1,22 +1,49 @@
 package com.example.justeat
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.SeekBar
+import android.widget.Switch
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.chip.Chip
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.checkbox.*
 
 class MainActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
+    //seekbar
+    var progressView: TextView? = null
+    var seekBarView: SeekBar? = null
 
-     var progressView: TextView? = null
-    var seekBarView : SeekBar?=null
+    //recycleView for checkbox
+    private val list: RecyclerView? = null
+    private var recyclerAdapter: adapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //switch button
+        val sw1 = findViewById<Switch>(R.id.switch1)
+
+        sw1?.setOnCheckedChangeListener { _, isChecked ->
+            val message = if (isChecked) "Switch:ON" else "Switch:OFF"
+            Toast.makeText(this@MainActivity, message,
+                Toast.LENGTH_SHORT).show()
+
+
+
+        }
+
 
         // Seekbar
         progressView = this.textView2
@@ -24,27 +51,97 @@ class MainActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
         seekBarView!!.setOnSeekBarChangeListener(this)
 
         // Set chip group checked change listener
-        chipGroup2.setOnCheckedChangeListener{group,checkedId:Int ->
+        chipGroup2.setOnCheckedChangeListener { group, checkedId: Int ->
             // Get the checked chip instance from chip group
-            val chip:Chip? = findViewById(checkedId)
+            val chip: Chip? = findViewById(checkedId)
 
             chip?.let {
                 // Show the checked chip text on toast message
                 toast("${it.text} checked")
             }
         }
+        //chip group
+        chipGroup4.setOnCheckedChangeListener { group, checkId: Int ->
 
-        chipGroup4.setOnCheckedChangeListener{group,checkId:Int ->
-
-            val chip:Chip? = findViewById(checkId)
-            chip?.let{
+            val chip: Chip? = findViewById(checkId)
+            chip?.let {
 
                 chip?.setChipBackgroundColorResource(R.color.lightBlue)
             }
         }
 
+        //checkBox
+        val list = findViewById<RecyclerView>(R.id.listCat)
+        val category = arrayListOf<String>()
+        category.add("All")
+        category.add("Malaysian Food")
+        category.add("Fast Food")
+        category.add("Indian")
+        category.add("Halal")
+        category.add("Western")
+        category.add("Chinese")
+
+        val layoutManager = LinearLayoutManager(this)
+        list.layoutManager = layoutManager
+        recyclerAdapter = adapter(this@MainActivity, category)
+        list.addItemDecoration(CustomDividerItemDecoration(this@MainActivity))
+        list.adapter = recyclerAdapter
+
+
     }
 
+    //checkBox Adapter
+    private inner class CustomDividerItemDecoration(context: Context) :
+        RecyclerView.ItemDecoration() {
+        private val drawableline: Drawable?
+
+        init {
+            drawableline =
+                ContextCompat.getDrawable(context,R.drawable.ic_check_box_black_24dp)
+        }
+
+        override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+            val left = parent.paddingLeft
+            val right = parent.width - parent.paddingRight
+            val childCount = parent.childCount
+            for (i in 0 until childCount) {
+                val child = parent.getChildAt(i)
+                val params = child.layoutParams as RecyclerView.LayoutParams
+                val top = child.bottom + params.bottomMargin
+                val bottom = top + drawableline!!.intrinsicHeight
+                drawableline.setBounds(left, top, right, bottom)
+                drawableline.draw(c)
+            }
+        }
+    }
+
+    private inner class adapter(internal var context: Context, internal var mData: List<String>) :
+        RecyclerView.Adapter<adapter.myViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): adapter.myViewHolder {
+            val view =
+                LayoutInflater.from(context).inflate(R.layout.checkbox, parent, false)
+            return myViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: adapter.myViewHolder, position: Int) {
+            holder.category.text = mData[position]
+        }
+
+        override fun getItemCount(): Int {
+            return mData.size
+        }
+
+        inner class myViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            internal var category: TextView
+
+            init {
+                category = itemView.findViewById(R.id.cat)
+            }
+        }
+    }
+
+    //seekBar
     override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
         progressView!!.text = progress.toString()
     }
